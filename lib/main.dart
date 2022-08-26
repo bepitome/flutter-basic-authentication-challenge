642476bb46173591034174cffbbc5a54b69040b5
ymaarfaj <37027715+ymaarfaj@users.noreply.github.com>
2022-08-26 05:33:26 +0300

    initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 void main() {
   /**
    *    Welcome to Basic authentication challenge
    *    Make sure to read CHALLENGE.md before you start
    *
    */
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        textTheme: const TextTheme(
+          headline6: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        buttonTheme: ButtonThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.blue,
+          ).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.deepOrange,
+        ),
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+        title: const Text('test'),
+      )),
+    );
+  }
 }
